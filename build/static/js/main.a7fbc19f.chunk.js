7aa83defe7412595b0215b0f3a5a77d13753f974
Celiamf <celiamartinfern@gmail.com>
2020-11-12 09:27:52 +0100

    Update build

---
+++ build/static/js/main.a7fbc19f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-react-project"]=this["webpackJsonpmy-react-project"]||[]).push([[0],{10:function(e,t,c){},11:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c(1),s=c.n(r),a=c(3),i=c.n(a);c(9),c(10);var o=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("header",{className:"App-header",children:Object(n.jsx)("h1",{children:"Mi lista de Pokemon"})})})};i.a.render(Object(n.jsx)(s.a.StrictMode,{children:Object(n.jsx)(o,{})}),document.getElementById("root"))},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.a7fbc19f.chunk.js.map
